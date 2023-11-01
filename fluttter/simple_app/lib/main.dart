fc9f235afc77796797e3f6aba1947afe9c5f0e3e
Nikola <nikolovski.nikola42@gmail.com>
2023-11-01 02:22:39 +0100

    basic flutter funct

--- fluttter/simple_app/lib/main.dart
+++ fluttter/simple_app/lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:simple_app/presentation/widgets/porfessor_widget.dart';
+import 'package:simple_app/presentation/screens/ProfessorListScreen.dart';
 
-import 'api/api_service.dart';
-import 'domain/models/professor.dart';
-
-void main() async {
-  final apiService = ApiService();
-  final List<Professor> professors = await apiService.fetchData();
-  runApp(MyApp(professors: professors));
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final List<Professor> professors;
-
-  const MyApp({required this.professors, Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,18 +13,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // Your theme configuration here...
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Demo Home Page'),
-        ),
-        body: Center(
-          child: ListView(
-            children: professors.map((professor) {
-              return ProfessorWidget(professor: professor);
-            }).toList(),
-          ),
-        ),
-      ),
+      home: ProfessorListScreen(),
     );
   }
 }
+
+
---
+++ fluttter/simple_app/lib/presentation/screens/ProfessorDetailsScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../domain/models/professor.dart';
+
+class ProfessorDetailsScreen extends StatelessWidget {
+  final Professor professor;
+
+  const ProfessorDetailsScreen({required this.professor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Professor Details')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Name: ${professor.name}'),
+          Text('Email: ${professor.email}'),
+          Text('Professor Title: ${professor.professorTitle}'),
+        ],
+      ),
+    );
+  }
+}
