916a81d4d1469842c211eb9e3bc39c62d764a501
anastazija-kovachevikj <anastazija.kovachevikj@gmail.com>
2024-02-09 18:29:21 +0100

    update calendar view

--- fluttter/simple_app/lib/presentation/schedule_mapper/schedule_widget.dart
+++ fluttter/simple_app/lib/presentation/schedule_mapper/schedule_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simple_app/presentation/schedule_mapper/slots/time_slot_widget.dart';
+import 'package:simple_app/presentation/schedule_mapper/slots/vertical_divider_widget.dart';
 
 import '../../domain/models/lecture.dart';
 import '../../domain/models/schedule.dart';
@@ -14,8 +15,10 @@ class ScheduleWidget extends StatelessWidget {
       {super.key, required this.schedule, required this.segmented});
 
   getDayColumns() {
-    List<ColumnScheduleWidget> days = [];
+    List<StatelessWidget> days = [];
     List<List<Lecture>> list = [];
+
+    days.add(TimeSlotWidget(startTimeHour: 8, endTimeHour: 20));
 
     for (int i = 0; i < 5; i++) {
       List<Lecture> lecture = [];
@@ -29,12 +32,16 @@ class ScheduleWidget extends StatelessWidget {
       list[idx].add(lectures[i]);
     }
 
+    days.add(VerticalDividerWidget());
+
     for (int i = 0; i < 5; i++) {
       days.add(ColumnScheduleWidget(
         lectures: list[i],
         day: i,
-        segmented: segmented, scheduleId: schedule.id ?? -1,
+        segmented: segmented,
+        scheduleId: schedule.id ?? -1,
       ));
+      if (i != 4) days.add(VerticalDividerWidget());
     }
 
     return days;
@@ -50,12 +57,10 @@ class ScheduleWidget extends StatelessWidget {
         child: Stack(children: [
           Container(
             padding: const EdgeInsets.all(5.0),
-            color: Colors.grey[200],
             child: Flex(
                 direction: Axis.horizontal,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TimeSlotWidget(startTimeHour: 8, endTimeHour: 19),
                   ...getDayColumns(),
                 ]),
           ),
