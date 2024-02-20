1d6e4bddd9df933a333ab6ef03564ac5c88eab78
anastazija-kovachevikj <anastazija.kovachevikj@gmail.com>
2024-02-20 19:20:40 +0100

    floating buttons for adding subjects

--- fluttter/simple_app/lib/presentation/screens/calendar_screen.dart
+++ fluttter/simple_app/lib/presentation/screens/calendar_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:simple_app/presentation/screens/list/choose_lecture_type_screen.dart';
@@ -8,14 +6,17 @@ import 'package:simple_app/service/schedule_service.dart';
 
 import '../../domain/models/schedule.dart';
 import '../schedule_mapper/schedule_widget.dart';
+import '../widgets/ActionButton.dart';
+import '../widgets/ExpandableFab.dart';
+import 'add/add_lecture_slot_screen.dart';
 import 'list/course_list_screen.dart';
 
 void main() {
-
   runApp(MaterialApp(
     home: CalendarScreen(1),
   ));
 }
+
 class CalendarScreen extends StatefulWidget {
   final int scheduleId;
 
@@ -48,111 +49,25 @@ class _CalendarAppState extends State<CalendarScreen> {
       theme: theme,
       home: Scaffold(
         appBar: AppBar(
-          // leading: IconButton(
-          //   icon: Icon(Icons.arrow_back),
-          //   onPressed: () {
-          //     Navigator.push(
-          //       context,
-          //       MaterialPageRoute(
-          //         builder: (context) => ScheduleListScreen(),
-          //
-          //       ),
-          //     );
-          //   },
-          // ),
-          title: const Text('Распоред',
-          style: TextStyle(
-            fontSize: 16,
-            color: Color(0xFF123499),
-          ),),
+          title: const Text(
+            'Распоред',
+            style: TextStyle(
+              fontSize: 16,
+              color: Color(0xFF123499),
+            ),
+          ),
           elevation: 20,
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Flex(
-              direction: Axis.horizontal,
-                mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: OutlinedButton(
-                    onPressed: () {
-                      // Navigate to a different screen when the button is pressed
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ChooseLectureScreen(schedule: scheduleFuture!),
-                        ),
-                      );
-                    },
-                    style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(8.0), // Set padding for the button
-                    minimumSize: const Size(100, 40),),
-                    child: const Row(
-                      children: [
-                        Icon(Icons.add, color: Colors.blueGrey), // Replace with your desired icon
-                        SizedBox(width: 8), // Adjust the spacing between icon and text
-                        Text('Додади предмет', style: TextStyle(color: Colors.blueGrey)),
-                      ],
-                    ),
-
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: OutlinedButton(
-
-                    onPressed: () {
-                      // Navigate to a different screen when the button is pressed
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ScheduleListScreen(),
-
-                        ),
-                      );
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Распоредот е зачуван'),
-                          duration: Duration(seconds: 2),
-                        ),
-                      );
-                    },
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.all(8.0), // Set padding for the button
-                      minimumSize: Size(100, 40),),
-                    child: const Row(
-                      children: [
-                        Icon(Icons.save_alt_outlined, color: Colors.blueGrey), // Replace with your desired icon
-                        SizedBox(width: 8), // Adjust the spacing between icon and text
-                        Text('Зачувај распоред', style: TextStyle(color: Colors.blueGrey)),
-                      ],
-                    ),
-                  ),
-                ),
-
-//i try
-                // Padding(
-                //   padding: const EdgeInsets.all(16.0),
-                //   child: ElevatedButton(
-                //     onPressed: () {
-                //       // Navigate to a different screen when the button is pressed
-                //       Navigator.push(
-                //         context,
-                //         MaterialPageRoute(
-                //           builder: (context) => CourseListScreen(schedule: scheduleFuture!),
-                //         ),
-                //       );
-                //     },
-                //     child: Text('Create custom lecture'),
-                //   ),
-                // ),
-              ]),
             Expanded(
               child: scheduleFuture != null
                   ? Center(
-                child: ScheduleWidget(schedule: scheduleFuture!, segmented:false,),
+                child: ScheduleWidget(
+                  schedule: scheduleFuture!,
+                  segmented: false,
+                ),
               )
                   : Center(
                 child: Padding(
@@ -168,8 +83,58 @@ class _CalendarAppState extends State<CalendarScreen> {
             ),
           ],
         ),
+        floatingActionButton: ExpandableFab(
+          distance: 112,
+          children: [
+            ActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => FieldScreen(schedule: scheduleFuture!),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.save),
+              //label: "Зачувај",
+            ),
+
+            ActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => FieldScreen(schedule: scheduleFuture!),
+                  ),
+                );
+                // ScaffoldMessenger.of(context).showSnackBar(
+                //   const SnackBar(
+                //     content: Text('Распоредот е зачуван'),
+                //     duration: Duration(seconds: 2),
+                //   ),
+                // );
+              },
+              icon: const Icon(Icons.dashboard_customize),
+              //label: "Custom",
+            ),
+            ActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CourseListScreen(schedule: scheduleFuture!),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.add_circle_outlined),
+              //label: "Постоечки",
+            ),
+
+          ],
+        ),
+
+
       ),
     );
   }
 }
-
