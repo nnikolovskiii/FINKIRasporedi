aedd3e90e6bfb05028c545f3f1a1021f2979bc74
Nikola <nikolovski.nikola42@gmail.com>
2024-06-29 16:49:29 +0200

    delete the previously named frontend flutter directory.

--- fluttter/simple_app/lib/service/shared_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class SharedService {
-
-  static Future<bool> isLoggedIn() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.containsKey('login_details');
-  }
-}
